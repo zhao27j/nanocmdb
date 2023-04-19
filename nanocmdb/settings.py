bb2a629115ffb26d2f17bc04c3c518cdb67357a0
Jun ZHAO <zhao27j@gmail.com>
2023-04-19 17:32:32 +0800

    get_ready_for_production

--- nanocmdb/settings.py
+++ nanocmdb/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-l=$7bc+(lkaf)!
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
